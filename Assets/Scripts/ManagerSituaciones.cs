252b54a03d34eb11b75c2cac9b05e6500ce20ca5
Ricardo <henry3197@hotmail.com>
2022-09-10 11:15:01 -0500

    evolucion de situaciones y stats
    el manager de situaciones recibe ahora 3 situaciones y 3 eventos especificos para cada situacion, en este caso estan los 3 eventos de recibir comida (Pedigree,Pollo y Chocolate), los eventos nuevos son EntregarPedigree, EntregarPollo y EntregarChocolate

--- Assets/Scripts/ManagerSituaciones.cs
+++ Assets/Scripts/ManagerSituaciones.cs
@@ -5,9 +5,38 @@ using UnityEngine;
 public class ManagerSituaciones : MonoBehaviour
 {
     [SerializeField] SituacionesYCambios[] situaciones;
+    [SerializeField] Eventos recibirPedigree, recibirPollo, recibirChocolate,alterarStats,actualizarStats;
     [SerializeField] Estadisticas mascota;
     void Start()
     {
-        situaciones[Random.Range(0, situaciones.Length)].AlterarStats(mascota);
+        recibirPedigree.GEvent += Pedigree;
+        recibirPollo.GEvent += Pollo;
+        recibirChocolate.GEvent += Chocolate;
+    }
+
+    void Pedigree()
+    {
+        situaciones[0].AlterarStats(mascota);
+        alterarStats.FireEvent();
+        actualizarStats.FireEvent();
+    }
+    void Pollo()
+    {
+        situaciones[1].AlterarStats(mascota);
+        alterarStats.FireEvent();
+        actualizarStats.FireEvent();
+    }
+    void Chocolate()
+    {
+        situaciones[2].AlterarStats(mascota);
+        alterarStats.FireEvent();
+        actualizarStats.FireEvent();
+    }
+
+    private void OnDestroy()
+    {
+        recibirPedigree.GEvent -= Pedigree;
+        recibirPollo.GEvent -= Pollo;
+        recibirChocolate.GEvent -= Chocolate;
     }
 }
