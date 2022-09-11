056170804af7409ce08c9016e8be2f9bc69f376b
Ricardo <henry3197@hotmail.com>
2022-09-11 18:34:14 -0500

    creditos
    la escena de creditos ya esta lista para acoplarse al main del juego

---
+++ Assets/Scripts/cargarEscena.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cargarEscena : MonoBehaviour
+{
+    [SerializeField] string nombreEscena;
+    public void CargarEscena()
+    {
+        SceneManager.LoadScene(nombreEscena);
+    }
+}
