4cad66ed63d267abf288c8a837d216d644bc4197
estebanmont <88239099+estebanmont@users.noreply.github.com>
2022-10-09 02:56:34 -0500

    Correccion Escena FAQ
    se corregio un errror al abrir y cerrar los mensajes del dogtor

--- Assets/ControllerTipsButon.cs
+++ Assets/ControllerTipsButon.cs
@@ -5,18 +5,56 @@ using UnityEngine.SceneManagement;
 
 public class ControllerTipsButon : MonoBehaviour
 {
-    [SerializeField] GameObject PanelTipComida;
+
+    [SerializeField] float duracionTip, aparicionTip;
+    [SerializeField] Vector2 posicionNueva, posicionInicial;
+    [SerializeField] GameObject PanelTipComida,Dogo1,Dogo2;
+
+    void Start()
+    {
+        Dogo1.transform.localScale = Vector2.zero;
+        Dogo1.transform.localPosition = posicionInicial;
+    }
+
     public void OpenFAQTip()
     {
         Time.timeScale = 0f;
         PanelTipComida.SetActive(true);
         //ButtonMenu.SetActive(false);
     }
-
     public void CloseFAQTip()
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 0f;
         PanelTipComida.SetActive(false);
+        //ButtonMenu.SetActive(false);
+    }
+
+
+    public void OpenDogoTIP()
+    {
+        Time.timeScale = 0f;
+        Dogo1.SetActive(true);
+        //ButtonMenu.SetActive(false);
+    }
+
+    public void CloseDogoTIP()
+    {
+        Time.timeScale = 1f;
+        Dogo1.SetActive(false);
+        //al ButtonMenu.SetActive(true);
+    }
+
+    public void OpenDogoTIP2()
+    {
+        Time.timeScale = 0f;
+        Dogo2.SetActive(true);
+        //ButtonMenu.SetActive(false);
+    }
+
+    public void CloseDogoTIP2()
+    {
+        Time.timeScale = 1f;
+        Dogo2.SetActive(false);
         //al ButtonMenu.SetActive(true);
     }
 
---
+++ Assets/DOGO1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOGO1 : MonoBehaviour
+{
+    
+    void Start()
+    {
+        transform.localScale = Vector2.one;
+      
+    }
+
+   
+
+
+}
